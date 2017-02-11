1ed52e0b9f4f66bcd25eadf59aafec75708843f1
Gabriel Faria <gggfaria@outlook.com>
2017-02-11 16:40:16 -0200

    Adicionando classe de teste para Eleitor

---
+++ ProjetoPolitica.Selenium.Teste/CasosTeste/EleitorTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjetoPolitica.Selenium.Teste.CasosTeste
+{
+    [TestFixture]
+    class EleitorTest
+    {
+        [Test]
+        public void ValidacaoEmail()
+        {
+            IWebDriver driver = new ChromeDriver();
+            Thread.Sleep(500);
+
+        }
+    }
+}
