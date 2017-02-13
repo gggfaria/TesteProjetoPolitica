c27adde7faee4a0235b97d705b417bfa781ded44
Gabriel Faria <gggfaria@outlook.com>
2017-02-12 22:19:40 -0200

    Adicionando teste EmailNaoCadastrado no cadastro de eleitor

--- ProjetoPolitica.Selenium.Teste/CasosTeste/EleitorTest.cs
+++ ProjetoPolitica.Selenium.Teste/CasosTeste/EleitorTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +15,32 @@ namespace ProjetoPolitica.Selenium.Teste.CasosTeste
     class EleitorTest
     {
         [Test]
-        public void ValidacaoEmail()
+        public void ValidarEmailNaoCadastrado()
         {
             IWebDriver driver = new ChromeDriver();
             Thread.Sleep(500);
+
+            driver.Navigate().GoToUrl("http://localhost:8080/Politica/");
+            IWebElement linkCadastrar = driver.FindElement(By.LinkText("Cadastrar"));
+            linkCadastrar.Click();
+
+            Thread.Sleep(500);
+
+            String emailGuid;
+            emailGuid = Guid.NewGuid().ToString() + "@mail.com";
+
+            IWebElement inputEmail = driver.FindElement(By.Id("nome_cadastro"));
+            IWebElement inputSenha = driver.FindElement(By.Id("senha"));
+
+            inputEmail.SendKeys(emailGuid);
+            inputSenha.SendKeys("");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("notifyjs-bootstrap-base")));
+
+            IWebElement divSucesso = driver.FindElement(By.ClassName("notifyjs-bootstrap-success"));
+
+            Assert.NotNull(divSucesso);
 
         }
     }
